aca16868465cd11e5440838ca59ff6a9a5159f58
Jihye Jegal <jihyejegal@Jihyeui-MacBook-Air.local>
2017-03-30 02:35:41 +0900

    add HoH basic concept

---
+++ History-of-HearthStone/History-of-HearthStone/History-of-Hearthstone.swift
@@ -0,0 +1,25 @@
+//
+//  History-of-Hearthstone.swift
+//  History-of-HearthStone
+//
+//  Created by Jihye Jegal on 2017. 3. 30..
+//  Copyright © 2017년 Jihye Jegal. All rights reserved.
+//
+
+import Foundation
+
+class Card {
+    
+}
+
+class CardPack {
+    
+}
+
+class Adventure {
+    
+}
+
+class Patch {
+    
+}
---
+++ History-of-HearthStone/History-of-HearthStone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  History-of-HearthStone
+//
+//  Created by Jihye Jegal on 2017. 3. 30..
+//  Copyright © 2017년 Jihye Jegal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
